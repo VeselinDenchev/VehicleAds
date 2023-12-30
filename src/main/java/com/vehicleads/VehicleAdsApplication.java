8495c1b2476dbeb4c2eb5e962f50844dcd92c468
Veselin Denchev <den4ev01@gmail.com>
2023-12-30 18:33:16 +0200

    Initial commit

---
+++ src/main/java/com/vehicleads/VehicleAdsApplication.java
@@ -0,0 +1,13 @@
+package com.vehicleads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleAdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleAdsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vehicleads/VehicleAdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehicleads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleAdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
